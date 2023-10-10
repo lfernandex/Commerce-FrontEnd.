cf719a0084eee1bca4ab1a25382d772c47ce5e88
root <root@Lucas>
2023-10-10 15:51:31 -0300

    Product CRUD static layout

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,8 +1,59 @@
+import "./styles.css";
+
 export default function ProductForm() {
 
-    return(
+    return (
         <>
-            <h1>Formulario de produtos</h1>
+            <main>
+                <section id="product-form-section" className="fb-container">
+                    <div className="fb-product-form-container">
+                        <form className="fb-card fb-form">
+                            <h2>Dados do Produto</h2>
+                            <div className="fb-form-controls-container">
+                                <div>
+                                    <input
+                                        className="fb-form-control"
+                                        type="text"
+                                        placeholder="Nome"
+                                    />
+                                </div>
+                                <div>
+                                    <input
+                                        className="fb-form-control"
+                                        type="text"
+                                        placeholder="Preço"
+                                    />
+                                </div>
+                                <div>
+                                    <input
+                                        className="fb-form-control"
+                                        type="text"
+                                        placeholder="Imagem"
+                                    />
+                                </div>
+
+                                <div>
+                                    <select className="fb-form-control fb-select" required>
+                                        <option value="" disabled selected>Categorias</option>
+                                        <option value="Valor1">Valor 1</option>
+                                        <option value="Valor2">Valor 2</option>
+                                    </select>
+                                </div>
+
+                                <div>
+                                    <textarea className="fb-form-control fb-textarea" placeholder="Descrição"></textarea>
+                                </div>
+                            </div>
+
+
+                            <div className="fb-product-form-buttons">
+                                <button type="reset" className="fb-btn fb-btn-white">Cancelar</button>
+                                <button type="submit" className="fb-btn fb-btn-blue">Salvar</button>
+                            </div>
+                        </form>
+                    </div>
+                </section>
+            </main>
         </>
     );
 
--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -1,8 +1,119 @@
+import editIcon from "../../../assets/Pen.svg";
+import deleteIcon from "../../../assets/Trash.svg";
+import notebookIcon from "../../../assets/notebook.jpg";
+import "./styles.css";
+
 export default function ProductListing() {
 
-    return(
+    return (
         <>
-            <h1>Deus é Maravilhoso</h1>
+            <main>
+                <section id="product-listing-section" className="fb-container">
+
+                    <h2 className="fb-section-title">Cadastro de produtos</h2>
+
+                    <div className="fb-btn-page-container fb-mt20">
+                        <div className="fb-btn-next-page">
+                            Novo
+                        </div>
+                    </div>
+
+                    <form className="fb-seacrch-bar fb-mt20">
+                        <button type="submit">🔎︎</button>
+                        <input type="text" placeholder="Nome do produto" />
+                        <button type="reset">🗙</button>
+                    </form>
+
+                    <table className="fb-table fb-mb20 fb-mt20">
+                        <thead>
+                            <th className="fb-tb576">ID</th>
+                            <th></th>
+                            <th className="fb-tb768">Preço</th>
+                            <th>Nome</th>
+                            <th></th>
+                            <th></th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="fb-tb576">341</td>
+                                <td><img className="fb-product-image" src={notebookIcon} alt="" /></td>
+                                <td className="fb-tb768">R$: 5.000,00</td>
+                                <td>Computador Gamer XT Plus Ultra</td>
+                                <td><img className="fb-product-listing-btn" src={editIcon} alt="" /></td>
+                                <td><img className="fb-product-listing-btn" src={deleteIcon} alt="" /></td>
+                            </tr>
+                            <tr>
+                                <td className="fb-tb576">341</td>
+                                <td><img className="fb-product-image" src={notebookIcon} alt="" /></td>
+                                <td className="fb-tb768">R$: 5.000,00</td>
+                                <td>Computador Gamer XT Plus Ultra</td>
+                                <td><img className="fb-product-listing-btn" src={editIcon} alt="" /></td>
+                                <td><img className="fb-product-listing-btn" src={deleteIcon} alt="" /></td>
+                            </tr>
+                            <tr>
+                                <td className="fb-tb576">341</td>
+                                <td><img className="fb-product-image" src={notebookIcon} alt="" /></td>
+                                <td className="fb-tb768">R$: 5.000,00</td>
+                                <td>Computador Gamer XT Plus Ultra</td>
+                                <td><img className="fb-product-listing-btn" src={editIcon} alt="" /></td>
+                                <td><img className="fb-product-listing-btn" src={deleteIcon} alt="" /></td>
+                            </tr>
+                            <tr>
+                                <td className="fb-tb576">341</td>
+                                <td><img className="fb-product-image" src={notebookIcon} alt="" /></td>
+                                <td className="fb-tb768">R$: 5.000,00</td>
+                                <td>Computador Gamer XT Plus Ultra</td>
+                                <td><img className="fb-product-listing-btn" src={editIcon} alt="" /></td>
+                                <td><img className="fb-product-listing-btn" src={deleteIcon} alt="" /></td>
+                            </tr>
+                            <tr>
+                                <td className="fb-tb576">341</td>
+                                <td><img className="fb-product-image" src={notebookIcon} alt="" /></td>
+                                <td className="fb-tb768">R$: 5.000,00</td>
+                                <td>Computador Gamer XT Plus Ultra</td>
+                                <td><img className="fb-product-listing-btn" src={editIcon} alt="" /></td>
+                                <td><img className="fb-product-listing-btn" src={deleteIcon} alt="" /></td>
+                            </tr>
+                            <tr>
+                                <td className="fb-tb576">341</td>
+                                <td><img className="fb-product-image" src={notebookIcon} alt="" /></td>
+                                <td className="fb-tb768">R$: 5.000,00</td>
+                                <td>Computador Gamer XT Plus Ultra</td>
+                                <td><img className="fb-product-listing-btn" src={editIcon} alt="" /></td>
+                                <td><img className="fb-product-listing-btn" src={deleteIcon} alt="" /></td>
+                            </tr>
+                            <tr>
+                                <td className="fb-tb576">341</td>
+                                <td><img className="fb-product-image" src={notebookIcon} alt="" /></td>
+                                <td className="fb-tb768">R$: 5.000,00</td>
+                                <td>Computador Gamer XT Plus Ultra</td>
+                                <td><img className="fb-product-listing-btn" src={editIcon} alt="" /></td>
+                                <td><img className="fb-product-listing-btn" src={deleteIcon} alt="" /></td>
+                            </tr>
+                            <tr>
+                                <td className="fb-tb576">341</td>
+                                <td><img className="fb-product-image" src={notebookIcon} alt="" /></td>
+                                <td className="fb-tb768">R$: 5.000,00</td>
+                                <td>Computador Gamer XT Plus Ultra</td>
+                                <td><img className="fb-product-listing-btn" src={editIcon} alt="" /></td>
+                                <td><img className="fb-product-listing-btn" src={deleteIcon} alt="" /></td>
+                            </tr>
+                            <tr>
+                                <td className="fb-tb576">341</td>
+                                <td><img className="fb-product-image" src={notebookIcon} alt="" /></td>
+                                <td className="fb-tb768">R$: 5.000,00</td>
+                                <td>Computador Gamer XT Plus Ultra</td>
+                                <td><img className="fb-product-listing-btn" src={editIcon} alt="" /></td>
+                                <td><img className="fb-product-listing-btn" src={deleteIcon} alt="" /></td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+
+
+                    <div className="fb-btn-next-page fb-mt20">Carregar mais</div>
+                </section>
+            </main>
         </>
     );
 
