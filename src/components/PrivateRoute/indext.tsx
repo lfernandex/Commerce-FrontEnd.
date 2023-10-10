ff391c67089f1a8d5e474667e2590cb51d3e1670
root <root@Lucas>
2023-10-09 23:44:37 -0300

    Access control route level

--- src/components/PrivateRoute/indext.tsx
+++ src/components/PrivateRoute/indext.tsx
@@ -1,13 +1,17 @@
 import { Navigate } from "react-router-dom";
-import { isAuthenticated } from "../../services/AuthService";
+import { RoleEnum } from "../../models/auth";
+import { hasAnyRoles, isAuthenticated } from "../../services/AuthService";
 
 type Props = {
     children: JSX.Element;
+    roles?: RoleEnum[];
 }
-
-export function PrivateRoute({ children }: Props) {
+export function PrivateRoute({ children, roles = [] }: Props) {
     if (!isAuthenticated()) {
         return <Navigate to="/login" />;
+    }
+    if (!hasAnyRoles(roles)) {
+        return <Navigate to="/catalog" />;
     }
     return children;
 }
